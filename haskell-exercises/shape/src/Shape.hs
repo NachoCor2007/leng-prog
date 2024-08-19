701f9c643a7d298e51c08c595a1f63184434feee
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2024-08-19 18:47:34 -0300

    Implemented Shape

--- haskell-exercises/shape/src/Shape.hs
+++ haskell-exercises/shape/src/Shape.hs
@@ -8,25 +8,25 @@ data Rectangle = Rectangle Point Point deriving (Eq, Show)
 
 -- A point from a tuple Pair
 point::(Double, Double) -> Point
-point = error "Implement it"
+point (newX, newY) = Point {x=newX, y=newY}
 
 -- The origin
 origin::Point
-origin = error "Implement it"
+origin = Point {x=0.0, y=0.0}
 
 -- Rectangle from a Tuple where (x0 y0) == origin
 rectangle::(Double, Double) -> Rectangle
-rectangle = error "Implement it" 
+rectangle (newX, newY) = Rectangle origin Point {x=newX, y=newY}
 
 base::Rectangle -> Double
-base = error "Implement it"
+base (Rectangle p0 p1) = x p1 - x p0
 
 height::Rectangle -> Double
-height = error "Implement it"
+height (Rectangle p0 p1) = y p1 - y p0
 
 -- Circle from radius
 circle::Double -> Circle
-circle = error "Implement it" 
+circle r = Circle origin r
 
 -- Clase Shift
 
@@ -34,13 +34,20 @@ class Shift a where
    shift::a -> (Double, Double) -> a
    
 instance Shift Point where
-   shift  = error "Implement it"
+   shift p (xShift, yShift) = Point (x p + xShift) (y p + yShift)
    
 instance Shift Rectangle where
-   shift  = error "Implement it"
+   shift (Rectangle p0 p1) xyShift = Rectangle (shift p0 xyShift) (shift p1 xyShift)
    
 instance Shift Circle where
-   shift  = error "Implement it"
+   shift (Circle p r) xyShift = Circle (shift p xyShift) r
    
 -- Define the Surface class
-   
+class Surface a where
+  surface::a -> Double
+
+instance Surface Rectangle where
+  surface r = base r * height r
+
+instance Surface Circle where
+  surface (Circle _ r) = pi * r ^ 2
