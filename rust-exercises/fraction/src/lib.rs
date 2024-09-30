f6939d5857c1ccd8fcc0878ee88eaa68544bd884
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2024-09-30 18:17:43 -0300

    Implemented fraction exercise and "un-ignored" a test.

--- rust-exercises/fraction/src/lib.rs
+++ rust-exercises/fraction/src/lib.rs
@@ -2,30 +2,53 @@ type Fraction = (i32, i32);
 
 /// Add 2 fractions
 pub fn add((n1, d1): Fraction, (n2, d2): Fraction) -> Fraction {
-    panic!("Implement it");
+    let n_a = n1 * d2;
+    let n_b = n2 * d1;
+
+    let new_n = n_a + n_b;
+    let new_d = d1 * d2;
+
+    simplify(new_n, new_d)
+//  (new_n, new_d)
 }
 
 /// Subtract 2 fractions
 pub fn sub((n1, d1): Fraction, (n2, d2): Fraction) -> Fraction {
-    panic!("Implement it");
+    add((n1, d1), (-n2, d2))
 }
 
 /// Multiply 2 fractions
 pub fn mul((n1, d1): Fraction, (n2, d2): Fraction) -> Fraction {
-    panic!("Implement it");
+    let new_n = n1 * n2;
+    let new_d = d1 * d2;
+
+    simplify(new_n, new_d)
+//  (new_n, new_d)
 }
 
 /// Divide 2 fractions
 pub fn divide((n1, d1): Fraction, (n2, d2): Fraction) -> Fraction {
-    panic!("Implement it");
+    mul((n1, d1), (d2, n2))
 }
 
 /// Calculate the Highest common factor between 2 numbers
 pub fn hcf(a: i32, b: i32) -> i32 {
-    panic!("Implement it");
+    if a == 0 {
+        return b
+    } else if b == 0 {
+        return a
+    }
+
+    let remainder = a % b;
+    if remainder == 0 {
+        b
+    } else {
+        hcf(b, remainder)
+    }
 }
 
 /// Create a fraction simplifying with the arguments simplified by the `hcf`
 pub fn simplify(n: i32, d: i32) -> Fraction {
-     (n, d)
+    let factor = hcf(n, d);
+    (n / factor, d / factor)
 }
