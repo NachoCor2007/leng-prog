56c1a1e649bb5bdad524a1a33abd4c90a0592aab
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2024-09-30 15:06:34 -0300

    Completed collatz conjecture exercise.

--- rust-exercises/collatz-conjecture/src/lib.rs
+++ rust-exercises/collatz-conjecture/src/lib.rs
@@ -1,4 +1,22 @@
 pub fn collatz(mut n: u64) -> Option<u64> {
-  None
+  let remainder = n % 2;
+  match remainder {
+    0 => {
+      let mut count = 0;
+      while n != 1 {
+        n = n / 2;
+        count += 1;
+      }
+      Some(count)
+    },
+    1 => {
+      let mut count = 0;
+      while n != 1 {
+        n = 3 * n + 1;
+        count += 1;
+      }
+      Some(count)
+    },
+    _ => None
+  }
 }
-
