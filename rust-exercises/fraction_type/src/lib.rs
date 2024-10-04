8da6b7e830795be2efc61fbc7d2f0c545240830f
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2024-10-04 10:06:53 -0300

    Implemented fraction exercise and "un-ignored" a test.

--- rust-exercises/fraction_type/src/lib.rs
+++ rust-exercises/fraction_type/src/lib.rs
@@ -4,13 +4,38 @@ use std::ops;
 pub struct Fraction(pub i32, pub i32);
 
 impl Fraction {
-    
+    pub fn add(&self, other: Fraction) -> Fraction {
+        let n_a = self.0 * other.1;
+        let n_b = other.0 * self.1;
+
+        let new_n = n_a + n_b;
+        let new_d = self.1 * other.1;
+
+        simplify(new_n, new_d)
+    }
+
+    pub fn sub(& self, other: Fraction) -> Fraction {
+        self.add(Fraction(-other.0, other.1))
+    }
+
+    pub fn mul(& self, other: Fraction) -> Fraction {
+        let new_n = self.0 * other.0;
+        let new_d = self.1 * other.1;
+
+        simplify(new_n, new_d)
+    }
+
+    pub fn divide(& self, other: Fraction) -> Fraction {
+        self.mul(Fraction(other.1, other.0))
+    }
 }
 
 impl ops::Add for Fraction {
     type Output = Fraction;
 
-    
+    fn add(self, rhs: Self) -> Self::Output {
+        Fraction::add(&self, rhs)
+    }
 }
 
 /// Calculate the Highest common factor between 2 numbers
