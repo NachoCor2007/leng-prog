08554cc7d204a3e1b5362fcb00566245cfdebff3
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2024-10-04 14:03:42 -0300

    Progress on Queen-attack exercise

--- rust-exercises/queen-attack/src/lib.rs
+++ rust-exercises/queen-attack/src/lib.rs
@@ -8,16 +8,42 @@ pub struct Queen {
 
 impl ChessPosition {
     pub fn new(rank: i32, file: i32) -> Option<Self> {
-        panic!("Implement me");
+        let position_not_in_range = Self::check_position_not_in_range(rank, file);
+        if position_not_in_range {
+            return None
+        }
+
+        Some(ChessPosition{0: rank, 1: file})
+    }
+
+    fn check_position_not_in_range(rank: i32, file: i32) -> bool {
+        let rank_not_in_range = (rank < 0) | (rank > 7);
+        let file_not_in_range = (file < 0) | (file > 7);
+        let position_not_in_range = rank_not_in_range | file_not_in_range;
+
+        position_not_in_range
     }
 }
 
 impl Queen {
     pub fn new(position: ChessPosition) -> Self {
-        panic!("Implement me");
+        Queen{position}
     }
 
     pub fn can_attack(&self, other: &Queen) -> bool {
-    panic!("Implement me");
-}
+    //     check vertical
+        let in_vertical: bool = Self::check_vertical(&self.position, &other.position);
+    //     check horizontal
+        let in_horizontal: bool = Self::check_horizontal(&self.position, &other.position);
+    //     check diagonal
+    //     check anti-diagonal
+    }
+
+    fn check_vertical(my_position: &ChessPosition, her_position: &ChessPosition) -> bool {
+        my_position.1 == her_position.1
+    }
+
+    fn check_horizontal(my_position: &ChessPosition, her_position: &ChessPosition) -> bool {
+        my_position.0 == her_position.0
+    }
 }
