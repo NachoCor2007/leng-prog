a780a37ab26cdc50b3545d10a4336d394fa669b4
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2024-10-07 18:47:14 -0300

    Implemented sum_of_multiples.

--- rust-exercises/sum-of-multiples/src/lib.rs
+++ rust-exercises/sum-of-multiples/src/lib.rs
@@ -1,3 +1,31 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    0
+    let mut i = 1;
+    let mut result = 0;
+    while i < limit {
+        let is_multiple = check_multiple(i, &factors);
+
+        if is_multiple {
+            result += i
+        }
+
+        i += 1
+    }
+
+    result
+}
+
+fn check_multiple(number: u32, factors: &[u32]) -> bool {
+    for factor in factors {
+        if *factor == 0 {
+            continue
+        }
+
+        let rem = number % factor;
+
+        if rem == 0 {
+            return true
+        }
+    }
+
+    false
 }
