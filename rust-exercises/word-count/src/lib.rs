b4b18b0f9183fbc86df339ec73b04769da1ab96b
NachoCor2007 <incorrearodriguez@mail.austral.edu.ar>
2024-10-14 18:31:41 -0300

    Implemented word-count exercise.

--- rust-exercises/word-count/src/lib.rs
+++ rust-exercises/word-count/src/lib.rs
@@ -2,5 +2,15 @@ use std::collections::HashMap;
 
 /// Count occurrences of words.
 pub fn word_count(phrase: &str) -> HashMap<String, u32> {
-  panic!("Implement me!")
+  let words = phrase.split_whitespace();
+  let mut map = HashMap::new();
+
+  for word in words {
+    let un_capitalized_word = word.to_lowercase();
+
+    let word_entry = map.entry(un_capitalized_word).or_insert(0);
+    *word_entry += 1;
+  }
+
+  map
 }
